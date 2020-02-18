c6a366e3e658c804b5148cdabf657d943933308e
Princess Itaya <taycornelius@att.net>
2020-02-18 10:39:38 -0500

    dbstuff

---
+++ models/schedule.sql
@@ -0,0 +1,13 @@
+drop database if exists GrindhubDB;
+
+create database GrindhubDB;
+
+use database GrindhubDB;
+
+create table schedule(
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+
+
+
+)
