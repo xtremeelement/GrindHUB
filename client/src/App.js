a9a5df29e823ae421d3fdea58f5138649af10751
Brandon Downhour <xtremeelement@gmail.com>
2020-02-18 11:14:07 -0500

    working app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Fam</h1>
+    </div>
+  );
+}
+
+export default App;
