6c74f144fe362aee5ce3bff76dcda218b25db77c
Brandon Downhour <xtremeelement@gmail.com>
2020-02-20 11:29:31 -0500

    more front end stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Schedule from "./components/ScheduleComponent";
 import AppHeader from "./components/smallerComponents/AppHeader";
-
+import TimeOff from "./components/TimeOffComponent";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 import UserProfile from "./components/UserProfileComponent";
 import FolderList from "./components/DocsComponent";
 import NewsAlerts from "./components/NewsComponent";
-
 
 // import InboxComp from "./components/InboxComponent";
 import DashComponent from "./components/DashComponent";
@@ -22,13 +21,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={DashComponent} />
           <Route path="/schedule" component={Schedule} />
+          <Route path="/timeoff" component={TimeOff} />
+          <Route path="/announcements" component={NewsAlerts} />
         </Switch>
       </Router>
-
-    
-      <NewsAlerts/>
-
-
     </div>
   );
 }
