ba24c0932ce15cbaecfacd6a2284794722e137bb
Princess Itaya <taycornelius@att.net>
2020-02-25 13:52:56 -0500

    more card styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Schedule from "./components/ScheduleComponent";
+import ScheduleComp from "./components/ScheduleComponent";
 import TimeOff from "./components/TimeOffComponent";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserProfile from "./components/UserProfileComponent";
@@ -23,7 +23,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={SignInSide} />
           <Route path="/dashboard/:id" component={DashComponent} />
-          <Route path="/schedule/:id" component={Schedule} />
+          <Route path="/schedule/:id" component={ScheduleComp} />
           <Route path="/timeoff/:id" component={TimeOff} />
           <Route path="/signin/:id" component={SignInSide} />
           <Route path="/documents/:id" component={Documents} />
